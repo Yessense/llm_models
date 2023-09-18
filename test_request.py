ba8de0cbf2c7b70f40ffebf65d8e31ec6389d82e
Ampiro <patrat2015@yandex.ru>
2023-09-18 13:35:00 -0400

    Test promt was modified

--- test_request.py
+++ test_request.py
@@ -1,6 +1,14 @@
 import requests
 url = "http://127.0.0.1:8080/generate"
-data = {'prompt': 'pick up fruit from the table'}
+promt = """
+    How would you hide a red apple on the table?
+    1. move_to("red apple"), 2. pick_up("red apple", "unspecified"), 3. move_to("table"), 4. put("red apple", "table"), 5. done().
+    How would you throw a red apple from the box on the chair?
+    1. move_to("red apple"), 2. pick_up("red apple", "box"), 3. move_to("chair"), 4. put("red apple", "chair"), 5. done().
+    How would you move a red apple from the table to the drawer?
+        
+"""
+data = {'prompt': promt}
 print(requests.post(url, json=data).json()['output'])
 
 
