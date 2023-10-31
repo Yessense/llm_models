3661e8bac3f55bbed00a44a2b6484d6bac43647b
Maxim Patratskiy <patrat2015@yandex.ru>
2023-10-31 20:51:42 +0300

    Updating test_request with image passing

--- test_request.py
+++ test_request.py
@@ -1,15 +1,23 @@
 import requests
-url = "http://127.0.0.1:8080/generate"
-promt = """
-    How would you hide a red apple on the table?
-    1. move_to("red apple"), 2. pick_up("red apple", "unspecified"), 3. move_to("table"), 4. put("red apple", "table"), 5. done().
-    How would you throw a red apple from the box on the chair?
-    1. move_to("red apple"), 2. pick_up("red apple", "box"), 3. move_to("chair"), 4. put("red apple", "chair"), 5. done().
-    How would you move a red apple from the table to the drawer?
-        
-"""
+import base64
+from PIL import Image
 
+url = "http://127.0.0.1:8080"
+img_path = "data/img.jpeg"
+promt = """Look on this image and describe it"""
+
+def main():
+    with open(img_path, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+    
+    data = {"text": promt, "image": encoded_string}
+    
+    request = requests.post(url+"/generate", json=data)
+    
+    print(request.json())
+    
+    model_name = requests.get(url+"/name").json()
+    print(f"Model name {model_name}")
 
 if __name__ == "__main__":
-    data = {'prompt': promt}
-    print(requests.post(url, json=data).json()['output'])
+    main()
