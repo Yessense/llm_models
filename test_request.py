85fb650d2c9946bef7ffcd07494689326f10fa1f
Maxim Patratskiy <patrat2015@yandex.ru>
2023-11-26 09:27:55 -0500

    Testing the requesting of agent_planner

--- test_request.py
+++ test_request.py
@@ -1,23 +1,34 @@
 import requests
 import base64
 from PIL import Image
+# import numpy as np
+import pickle
+import codecs
+from typing import Any 
 
-url = "http://127.0.0.1:8080"
-img_path = "data/1-sobaki-1.jpg"
-promt = """What does it mean to love an animanl?"""
+url = "http://127.0.0.1:8083"
+img_path = "data/2_200.png"
+promt = """Describe image in detail"""
+
+
+def obj2str(obj: Any):
+    raw_data = codecs.encode(pickle.dumps(obj, protocol=pickle.HIGHEST_PROTOCOL), "base64").decode('latin1')
+    return raw_data
 
 def main():
-    with open(img_path, "rb") as image_file:
-        encoded_string = base64.b64encode(image_file.read()).decode()
+    image = Image.open(img_path)
+    raw_image = obj2str(image)
+    data = {"goal": promt, "image": raw_image, "task_type": 1}
     
-    print("With decode")
-    data = {"text": promt, "image": encoded_string}
+    # image = Image.open(img_path)
+    # arr_image = np.array(image).tolist()
+    # data = {"text": promt, "image": None}
     
-    request = requests.post(url+"/generate", json=data).json()
-    model_name = requests.get(url+"/name").json()
+    request = requests.post(url+"/add_llp_task", json=data).json()
+    # model_name = requests.get(url+"/name").json()
     
     print(request)
-    print(model_name)
+    # print(model_name)
 
 if __name__ == "__main__":
     main()
